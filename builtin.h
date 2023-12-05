2a550671925a24782d4a98b276e239a90cdfee49
hnagasak <hnagasak@student.42tokyo.jp>
2023-12-05 21:12:03 +0900

    create echo.c

---
+++ builtin.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 14:23:31 by hnagasak          #+#    #+#             */
+/*   Updated: 2023/12/05 14:27:39 by hnagasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_H
+# define BUILTIN_H
+
+# include <stddef.h>
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+#endif
