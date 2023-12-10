c2792188516528437ca0575c43efb8234f1ea450
keitaogitsu <ogitsukeita@gmail.com>
2023-12-10 10:42:01 +0900

    libft追加

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,41 @@
 /*   By: kogitsu <kogitsu@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 14:40:15 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/11/26 14:58:00 by kogitsu          ###   ########.fr       */
+/*   Updated: 2023/12/09 21:12:17 by kogitsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
 
-int	main(int argc, char **argv, char **envp)
+void next_line()
 {
-    printf("Hello minishell.");
+	
+}
+
+int main(int argc, char **argv, char **envp)
+{
+
+	signal(SIGINT, next_line);
+	// hello
+	char *line = NULL;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (line == NULL || strlen(line) == 0)
+		{
+			free(line);
+			break;
+		}
+		printf("line is '%s'\n", line);
+		add_history(line);
+		free(line);
+	}
+	printf("exit\n");
 	return (0);
 }
