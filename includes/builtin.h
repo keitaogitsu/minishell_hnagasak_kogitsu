621cd7115c58d04ba7cd3f01fad0fa024055d9cf
1RO8s <saki0220hiro@gmail.com>
2023-12-21 11:13:43 +0900

    exit.c用にbultin.hを修正

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,13 +6,15 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 14:23:31 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/12/17 05:58:37 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/12/19 18:24:22 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
+# include "libft.h"
+# include <dirent.h>
 # include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -24,10 +26,11 @@
 void	ft_echo(char *argv[]);
 void	ft_cd(char *argv[]);
 void	ft_pwd(void);
+void	ft_exit(char *argv[]);
 
 // libft
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	**ft_split(char const *s, char c);
-char	*ft_strjoin(char const *s1, char const *s2);
+// int		ft_strncmp(const char *s1, const char *s2, size_t n);
+// char	**ft_split(char const *s, char c);
+// char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
