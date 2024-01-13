148fb1ab79f42b7925dfe9f6a10875aefd1274c3
1RO8s <saki0220hiro@gmail.com>
2024-01-13 16:39:20 +0900

    print.cの修正

--- srcs/debug/print.c
+++ srcs/debug/print.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 17:27:01 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/01/04 17:31:06 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/01/11 05:50:45 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,54 @@ void	print_env(t_dlist *env_list)
 		current = current->nxt;
 	}
 	printf("--- end print_env ----\n");
+}
+
+char *tokentype2str(t_token_type type)
+{
+	if (type == CHAR_GENERAL)
+		return ("CHAR_GENERAL");
+	else if (type == CHAR_PIPE)
+		return ("CHAR_PIPE");
+	else if (type == CHAR_QUOTE)
+		return ("CHAR_QUOTE");
+	else if (type == CHAR_DQUOTE)
+		return ("CHAR_DQUOTE");
+	else if (type == CHAR_WHITESPACE)
+		return ("CHAR_WHITESPACE");
+	else if (type == CHAR_ESCAPE)
+		return ("CHAR_ESCAPE");
+	else if (type == CHAR_GREATER)
+		return ("CHAR_GREATER");
+	else if (type == CHAR_LESSER)
+		return ("CHAR_LESSER");
+	else if (type == CHAR_TAB)
+		return ("CHAR_TAB");
+	else if (type == CHAR_NULL)
+		return ("CHAR_NULL");
+	else if (type == D_GREATER)
+		return ("D_GREATER");
+	else if (type == D_LESSER)
+		return ("D_LESSER");
+	else
+		return ("UNKNOWN");
+}
+
+/**
+ * @brief トークンリストを出力する関数。
+ *
+ * この関数は、与えられたトークンリストを順に走査し、各トークンの内容（文字列とタイプ）を
+ * 標準出力に表示します。リストの末尾に達するまで、リストの各ノードに対してこの操作を繰り返します。
+ *
+ * @param tokens 出力するトークンのリスト。t_token型のポインタです。
+ */
+void	print_tokens(t_token *tokens)
+{
+	char *str_type;
+	while (tokens != NULL)
+	{
+		str_type = tokentype2str(tokens->type);
+		// printf("token: %s type:%d\n", tokens->str, tokens->type);
+		printf("token: %s type:%s\n", tokens->str, str_type);
+		tokens = tokens->next;
+	}
 }
