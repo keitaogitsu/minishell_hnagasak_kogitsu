7fcae9db701f592564a33e7dec03ef3c73b91c03
1RO8s <saki0220hiro@gmail.com>
2024-01-13 16:30:29 +0900

    t_dlistにiを追加

--- srcs/utils/list.c
+++ srcs/utils/list.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 12:40:45 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/01/04 17:32:22 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/01/10 17:32:51 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void	ft_dlstadd_back(t_dlist **lst, t_dlist *new)
 	if (*lst == NULL)
 	{
 		*lst = new;
+		new->i = 0;
 		return ;
 	}
 	last = *lst;
@@ -41,4 +42,37 @@ void	ft_dlstadd_back(t_dlist **lst, t_dlist *new)
 		last = last->nxt;
 	last->nxt = new;
 	new->prv = last;
+	new->i = last->i + 1;
+}
+
+/**
+ * @brief Set the index for each element in a doubly linked list.
+ * This function iterates through a doubly linked list and assigns an
+ * index to each element. The index starts at 0 and increments by 1
+ * for each successive element in the list.
+ *
+ * @param lst A pointer of the first element of the doubly linked list.
+ * The list is assumed to be non-NULL and properly initialized.
+ * Elements of the list should have a member 'i' to store the index,
+ * and a member 'nxt' which is a pointer to the next element in the list.
+ *
+ * @note The function does not return a value.
+ * It directly modifies the 'i' member
+ * of each element in the list to set the index.
+ * It is the caller's responsibility to ensure that the list is non-empty
+ * and each element of the list is properly allocated.
+ */
+void	set_lstidx(t_dlist **lst)
+{
+	t_dlist	*current;
+	size_t	i;
+
+	current = *lst;
+	i = 0;
+	while (current != NULL)
+	{
+		current->i = i;
+		current = current->nxt;
+		i++;
+	}
 }
