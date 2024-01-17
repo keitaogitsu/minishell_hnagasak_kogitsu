331bf851e0fc4a63df7510140e23171313f4cb42
1RO8s <saki0220hiro@gmai.com>
2024-01-17 15:39:07 +0900

    init_env関数の修正

--- srcs/utils/t_env.c
+++ srcs/utils/t_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   t_env.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 01:45:42 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/12/29 20:42:19 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/01/17 15:37:27 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,7 +108,7 @@ t_dlist	**init_env(char **envp)
 	{
 		// printf("envp[%d]: %s\n", i, envp[i]);
 		if (i == 0)
-			ft_dlstnew(to_env(envp[i], 0));
+			*env_list = ft_dlstnew(to_env(envp[i], 0));
 		else
 			ft_dlstadd_back(env_list, ft_dlstnew(to_env(envp[i], 0)));
 		i++;
