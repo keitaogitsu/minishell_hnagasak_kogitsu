9cb4bbde9d71cc988b08c295f9d7e2f4354c5825
1RO8s <saki0220hiro@gmail.com>
2024-02-15 06:44:12 +0900

    mainloop関数内でfreeする位置を修正

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kogitsu <kogitsu@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 14:40:15 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/02/11 18:27:59 by kogitsu          ###   ########.fr       */
+/*   Updated: 2024/02/15 06:43:51 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,19 +44,17 @@ void free_envlist(t_dlist **envlist)
 	current = *envlist;
 	while (current != NULL)
 	{
-		printf("---free_envlist---\n");
 		tmp = current;
 		env = (t_env *)tmp->cont;
-		printf("env->key:%s\n", env->key);
-		printf("env->value:%s\n", env->value);
+		printf("---free_envlist [%s]---\n",env->key);
+		// printf("env->key:%s\n", env->key);
+		// printf("env->value:%s\n", env->value);
 		free(env->key);
 		env->key = NULL;
 		free(env->value);
 		env->value = NULL;
 		free(env);
 		env = NULL;
-		// free(tmp->prv);
-		// tmp->prv = NULL;
 		current = current->nxt;
 		free(tmp);
 		tmp = NULL;
@@ -77,8 +75,7 @@ void	mainloop(char *line, t_dlist **env_list)
 			free(line);
 			break ;
 		}
-		// test_builtin(ft_split(line, ' '), env_list);
-		// cmd_list = gen_cmd_list(ft_split(line,"|"),env_list);
+
 		add_history(line);
 		tokens = tokenize(line);
 		free(line);
@@ -86,19 +83,15 @@ void	mainloop(char *line, t_dlist **env_list)
 			printf("## is command line\n");
 		else
 			printf("## is NOT command line\n");
-		// 変数展開
 		tokens = expand_env(tokens, env_list);
-		printf("## after expand_env\n");
-		printf("toknes_adress:%s\n", tokens->str);
 		print_tokens(tokens);
 		
 		cmd_list = create_cmd_list(tokens, env_list);
-		// ここまで使ったt_token：tokensはfreeする
-		free_tokens(tokens);
-		free_envlist(env_list);
 		// コマンド実行
 		// exec_cmd_list(cmd_list, env_list);
 		// print_cmd_list(cmd_list);
+		// ここまで使ったt_token：tokensはfreeする
+		free_tokens(tokens);
 		// free(line);
 		break;
 	}
@@ -146,5 +139,6 @@ int	main(int argc, char **argv, char **envp)
 	env_list = init_env(envp);
 	// print_envlist(env_list);
 	mainloop(line, env_list);
+	free_envlist(env_list);
 	return (0);
 }
