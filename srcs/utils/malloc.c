d6f2a14d3981c03c21ea6c80dc46bcce1706a18d
1RO8s <saki0220hiro@gmail.com>
2024-03-02 19:14:18 +0900

    malloc.cにutils.hのinclude追加

--- srcs/utils/malloc.c
+++ srcs/utils/malloc.c
@@ -6,11 +6,12 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 12:15:09 by kogitsu           #+#    #+#             */
-/*   Updated: 2024/03/02 19:03:13 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/03/02 19:13:18 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
+#include "utils.h"
 
 void	malloc_error_exit(void)
 {
