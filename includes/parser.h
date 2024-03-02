6d5250343c219c7ebb463cffb60fdef07ad34d21
1RO8s <saki0220hiro@gmail.com>
2024-03-02 19:20:31 +0900

    includesのnorm対応

--- includes/parser.h
+++ includes/parser.h
@@ -6,21 +6,21 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 14:21:28 by kogitsu           #+#    #+#             */
-/*   Updated: 2024/02/28 00:23:57 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/03/02 19:19:34 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-# include "utils.h"
-# include "lexer.h"
 # include "exec.h"
+# include "lexer.h"
+# include "utils.h"
 
-size_t  get_argv_len(t_token *token);
-t_redir *crt_redir(char *file, t_redir_type type);
-t_cmd   *cmd_init(t_token *tokens, t_dlist **env_list);
+size_t	get_argv_len(t_token *token);
+t_redir	*crt_redir(char *file, t_redir_type type);
+t_cmd	*cmd_init(t_token *tokens, t_dlist **env_list);
 // void    cmd_output_join(t_cmd *cmd, t_redir *redir);
-t_dlist **create_cmd_list(t_token *tokens, t_dlist **env_list);
+t_dlist	**create_cmd_list(t_token *tokens, t_dlist **env_list);
 
 #endif
