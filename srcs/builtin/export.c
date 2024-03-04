af02d2ce40c153ceab3ea2547a3931b4375f6d58
1RO8s <saki0220hiro@gmail.com>
2024-03-04 19:13:48 +0900

    exportコマンドに引数チェック追加
    - 1文字目が数字または=の場合、エラー
    - KEYに英数字、_以外が含まれる場合、エラー

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,18 +6,42 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 09:34:31 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/02/29 19:25:36 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/03/04 19:06:14 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 #include "util.h"
 
+static int	is_invalid_arg(char **argv)
+{
+	char	*key;
+	char	*value;
+
+	if (ft_isdigit(argv[1][0]) || argv[1][0] == '=')
+	{
+		ft_errmsg("minishell: export: not a valid identifier\n");
+		return (1);
+	}
+	get_key_value(argv[1], &key, &value);
+	while (*key)
+	{
+		if (!ft_isalnum(*key) && *key != '_')
+		{
+			ft_errmsg("minishell: export: not a valid identifier2\n");
+			return (1);
+		}
+		key++;
+	}
+	return (0);
+}
+
 void	ft_export(char *argv[], t_dlist **env_list)
 {
 	char	*str_env;
 	t_env	*env;
 
+	is_invalid_arg(argv);
 	str_env = argv[1];
 	if (!find_existing_env(str_env, env_list))
 	{
