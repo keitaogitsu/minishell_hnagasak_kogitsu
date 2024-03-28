8d9ad5087eb66f3627b2e2171e3d45bb89fd02c3
1RO8s <saki0220hiro@gmail.com>
2024-03-28 21:06:18 +0900

    不要な関数削除など

--- srcs/utils/path_validator.c
+++ srcs/utils/path_validator.c
@@ -6,35 +6,13 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 01:45:42 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/03/28 09:17:52 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/03/28 21:03:59 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "debug.h"
 #include "utils.h"
 #include <sys/stat.h>
-
-int	is_executable_file(char *path)
-{
-	struct stat	buf;
-
-	if (stat(path, &buf) == -1)
-		return (0);
-	if (S_ISREG(buf.st_mode) && (buf.st_mode & S_IXUSR))
-		return (1);
-	return (0);
-}
-
-int	is_updatable_file(char *path)
-{
-	struct stat	buf;
-
-	if (stat(path, &buf) == -1)
-		return (0);
-	if (S_ISREG(buf.st_mode) && (buf.st_mode & S_IWUSR))
-		return (1);
-	return (0);
-}
 
 int	is_directory(const char *path)
 {
