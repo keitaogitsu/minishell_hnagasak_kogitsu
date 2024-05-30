d13f81512313bb110953d4fa9dbb9564354108f5
1RO8s <saki0220hiro@gmail.com>
2024-05-30 20:36:32 +0900

    #33 の対応

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,33 +6,40 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 18:23:23 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/03/28 08:36:40 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/05/30 20:36:09 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 #include "exit_status.h"
 
-static int	is_invalid_arg(char **argv)
+static int	is_numeric_arg(char *arg)
 {
 	size_t	i;
 
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		if (i == 0 && (arg[i] == '-' || arg[i] == '+'))
+			i++;
+		if (!ft_isdigit(arg[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	is_invalid_arg(char **argv)
+{
+	if (get_argc(argv) > 1 && !is_numeric_arg(argv[1]))
+	{
+		ft_errmsg("minishell: exit: numeric argument required\n");
+		return (STATUS_NO_NUMERIC_ARG);
+	}
 	if (get_argc(argv) > 2)
 	{
 		ft_errmsg("minishell: exit: too many arguments\n");
 		return (1);
-	}
-	i = 0;
-	while (argv[1] != NULL && argv[1][i] != '\0')
-	{
-		if (i == 0 && (argv[1][i] == '-' || argv[1][i] == '+'))
-			i++;
-		if (!ft_isdigit(argv[1][i]))
-		{
-			ft_errmsg("minishell: exit: numeric argument required\n");
-			return (STATUS_NO_NUMERIC_ARG);
-		}
-		i++;
 	}
 	return (0);
 }
@@ -43,9 +50,11 @@ int	ft_exit(char *argv[])
 
 	printf("exit\n");
 	exit_status = is_invalid_arg(argv);
-	if (exit_status != 0)
+	if (exit_status == STATUS_NO_NUMERIC_ARG)
+		exit(STATUS_NO_NUMERIC_ARG);
+	else if (exit_status != 0)
 		return (exit_status);
-	if (argv[1] != NULL)
+	else if (argv[1] != NULL)
 	{
 		exit_status = ft_atoi(argv[1]);
 		exit(exit_status);
